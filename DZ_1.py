a53870b86c0d8dd1e9c11a3ab7720878a13db15f
Vitalii <vagontravki16@gmail.com>
2022-04-19 15:32:40 +0300

    1

---
+++ DZ_1.py
@@ -0,0 +1,11 @@
+odd_to_15 = (i for i in range(1, 16, 2))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+
+print(type(odd_to_15))
